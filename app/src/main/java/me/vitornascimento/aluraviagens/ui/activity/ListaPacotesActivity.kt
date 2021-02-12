f80f02c39382692553a9da43c1ac5a9aa90cec63
Vitor Nascimento <bielvitor2008@hotmail.com>
2021-02-12 17:30:46 -0300

    Implementado view binding.

--- app/src/main/java/me/vitornascimento/aluraviagens/ui/activity/ListaPacotesActivity.kt
+++ app/src/main/java/me/vitornascimento/aluraviagens/ui/activity/ListaPacotesActivity.kt
@@ -3,10 +3,15 @@ package me.vitornascimento.aluraviagens.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import me.vitornascimento.aluraviagens.R
+import me.vitornascimento.aluraviagens.databinding.ActivityListaPacotesBinding
 
 class ListaPacotesActivity : AppCompatActivity() {
+    lateinit var binding: ActivityListaPacotesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lista_pacotes)
+        binding = ActivityListaPacotesBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
